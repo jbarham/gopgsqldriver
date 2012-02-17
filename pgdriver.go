11485a00296d7472a3f64c43680fc1cd8da9018e
John Barham <jbarham@gmail.com>
2012-02-18 07:51:29 +1100

    Doc update

--- pgdriver.go
+++ pgdriver.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// pgsqldriver is a PostgreSQL driver for the experimental Go SQL database package.
+// Package pgsqldriver is a PostgreSQL driver for the Go SQL database package.
 package pgsqldriver
 
 /*
